88119e0fd53afc18a8cf96c4d6074039036cb65b
Linus <limpan.ml@hotmail.com>
2021-05-02 12:20:52 +0200

    La till en fil för övriga scripts

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Övrig javascript på webbapplikationen*/
+
+/*Funktion för uppladdning av bilder (EJ KLAR) */
+function uploadImage(imageName){
+    console.log('Uppladdning färdig av: '+imageName);
+}
+uploadImage('Test');
