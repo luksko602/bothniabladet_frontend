602189064da8ddcbe0d533059b0f54405c0feb8f
Linus <limpan.ml@hotmail.com>
2021-05-04 21:53:24 +0200

    Påbörjat koppling för att visa info från memberentiteten i backend

--- frontendreact/src/App.js
+++ frontendreact/src/App.js
@@ -1,14 +1,52 @@
-import React, { Component } from 'react';
+import React, {useState } from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
+import axios from 'axios';
+
+class Member extends React.Component{
+    
+    apiURL = ''; //URL till API
+    state = {
+        members: [] //Tom medlemslista som sedan fylls på med
+        //info ur medlemsentiteten i mysqldatabasen
+    }
+
+    componentDidMount() {
+        axios.get(this.apiURL)
+        .then(result => {
+            const members = result.data;
+            this.setState({members});
+        })
+    }
+
+    render(){
+        return( 
+        
+        <ul>
+            { this.state.members.map(member => 
+            <li> {member.ID_member}</li>)}
+        </ul>
+        );
+    }
+}
+
+ReactDOM.render(<Member/>, document.getElementById('root'))
 
 class App extends React.Component {
-  
-  }
+    
+    render(){
+        return <h1> TEST </h1>
+        
+    }
+}
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = new App();
+app.render();
+
+//ReactDOM.render(<app />, document.getElementById('root'));
 
 
 
-export default Component;
+//export default App;
 
--- frontendreact/src/index.js
+++ frontendreact/src/index.js
@@ -2,45 +2,43 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
 
-
-
-//STARTSIDAN
-class StartPage extends React.Component{
-
-  render(){
-    return <div class="topleftmenu">
-        
-        <div id="startpagelink"> 
-            <h1> <a href="index.html"> Bothniabladet</a> </h1>
-        </div>
-        
-        <div id="explorelink"> 
-            <h2> <a href="utforska.html"> Utforska </a> </h2>
-        </div>
-        
-        <div id="gallerylink"> 
-            <h2> <a href="bildgalleri.html">Bildgalleri </a></h2>
-        </div>  
-        
-        <div id="contactlink"> 
-            <h2> <a href="kontakt.html"> Kontakt </a> </h2>
-        </div>
-    </div> ;
-
+class Car extends React.Component {
+  render() {
+    return <h2> Hi, I am a Car! </h2>
   }
+}
+ReactDOM.render(<Car />, document.getElementById('root'))
 
- 
-  //Metod för att koppla till databasen i backend
-  connectToDB(){
+// //STARTSIDAN
+// class StartPage extends React.Component{
 
-  }
-};
+//   render(){
+//     return <div class="topleftmenu">
+        
+//         <div id="startpagelink"> 
+//             <h1> <a href="index.html"> Bothniabladet</a> </h1>
+//         </div>
+        
+//         <div id="explorelink"> 
+//             <h2> <a href="utforska.html"> Utforska </a> </h2>
+//         </div>
+        
+//         <div id="gallerylink"> 
+//             <h2> <a href="bildgalleri.html">Bildgalleri </a></h2>
+//         </div>  
+        
+//         <div id="contactlink"> 
+//             <h2> <a href="kontakt.html"> Kontakt </a> </h2>
+//         </div>
+//     </div> ;
 
-const startsida = new StartPage();
-startsida.render();
+//   }
+// };
 
-ReactDOM.render(<StartPage/>, document.getElementById('root'));
+// const startsida = new StartPage();
+// startsida.render();
 
+// ReactDOM.render(<StartPage/>, document.getElementById('root')); 
 
 
 /*ReactDOM.render(
