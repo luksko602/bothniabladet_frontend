6453657d01c6efe348b72b8c439e4ab03a88fa9d
Linus <limpan.ml@hotmail.com>
2021-05-07 13:50:40 +0200

    Påbörjade kod för fetch av bilder. La till lite css för footer

--- frontendreact/src/App.js
+++ frontendreact/src/App.js
@@ -6,13 +6,28 @@ import TopRightMenu from './Components/TopRightMenu';
 import Footer from './Components/Footer';
 import axios from 'axios'; 
 
+
  
 function App() {
-
   // //Kod för fetch och display av en medlem ur api. 
   const[member, setMember] = useState(null);
   const[loading, setLoading] = useState(true);
-  useEffect(async() => {
+  
+  //Fetch av bilder
+  var fetchURL = 'http://localhost:8080/bothniabladet/Bothniabladet_backend/server/api/image/read.php';
+  fetch(fetchURL)
+  .then(res => res.blob())
+  .then(blob => {
+    let img = document.createElement('img');
+    img.src = URL.createObjectURL(blob);
+    document.querySelector("body").appendChild(img);
+  })
+
+
+
+ //useEffect för medlemshämtning
+  useEffect(async() => {  
+    //Hämta medlem genom fetch
     const response = await fetch("http://localhost:8080/bothniabladet/Bothniabladet_backend/server/api/member/read.php");
     const datainfo = await response.json();
     const[item] = datainfo.data;
@@ -27,9 +42,12 @@ function App() {
     <TopRightMenu/>
     <Footer/>
 
+    
+    
     <div>
       {loading ? <div> ...laddar medlem </div> : <div>{member.first_name} </div>}  
     </div>
+
     </React.Fragment>
     
   );
